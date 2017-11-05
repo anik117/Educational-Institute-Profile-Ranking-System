4ecd3cefdc7f96cd00ab3c72e09546454dc4b22c
Tanvir Ahassan Anik <anik880@gmail.com>
2017-11-06 00:37:58 +0600

    App name changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'EIPRS') }}</title>
+    <title>{{ config('app.name', 'SRS') }}</title>
     
     {{-- Font awesome cdn --}}
 		<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
